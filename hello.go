7819dc494ea97f744f2554575b36fcb2a60f8af7
abhyuditraj388 <abhyudit.raj@cars24.com>
2025-11-19 23:03:25 +0530

    ninth

--- hello.go
+++ hello.go
@@ -4,13 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
 )
 
 func main() {
 	x := bufio.NewScanner(os.Stdin)
 	x.Scan()
-	y, _ := strconv.ParseInt(x.Text(), 10, 64)
-	fmt.Println(int32(y))
+	fmt.Println(x.Text())
 
 }
