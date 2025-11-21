66316703efcce025f8f64f20b13715589dffca83
abhyuditraj388 <abhyudit.raj@cars24.com>
2025-11-21 15:29:21 +0530

    12th

--- hello.go
+++ hello.go
@@ -1,15 +1,20 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+import "fmt"
+
+type dog struct {
+	name string
+	age  int
+}
+
+func (d dog) bark() string {
+	return d.name + " is barking"
+}
 
 func main() {
-	x := bufio.NewScanner(os.Stdin)
-	x.Scan()
 
-	fmt.Println(x.Text())
+	tommy := dog{"tommy", 25}
+
+	fmt.Println(tommy.bark())
 
 }
